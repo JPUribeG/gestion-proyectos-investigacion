ebabaa88967d7611c155b8b04cf1e57afb296003
Juan Pablo <Juan Pablo@192.168.1.194>
2022-04-03 16:58:26 -0500

    Commit inicialo con Eclipse

---
+++ gestion-proyectos-investigacion/src/main/java/com/example/gestionproyectosinvestigacion/GestionProyectosInvestigacionApplication.java
@@ -0,0 +1,13 @@
+package com.example.gestionproyectosinvestigacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionProyectosInvestigacionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionProyectosInvestigacionApplication.class, args);
+	}
+
+}
---
+++ gestion-proyectos-investigacion/src/test/java/com/example/gestionproyectosinvestigacion/GestionProyectosInvestigacionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gestionproyectosinvestigacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionProyectosInvestigacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
